f4800a3ad8f176bc5f8a1ead2b9d5874a39d7d61
Giorgos Kallis <giorgos.kallis.gr@gmail.com>
2026-05-07 13:04:22 +0300

    feat: add About section

---
+++ components/sections/about.js
@@ -0,0 +1,12 @@
+import MonoCard from '../mono-card'
+
+const About = () => (
+  <MonoCard label="about" id="about">
+    Senior Full-Stack Engineer with 7+ years building production software.
+    Currently at Pitcher AG, shipping B2B SaaS to Fortune 500 sales
+    organizations. Backend-leaning, comfortable across the stack, focused
+    on systems that hold up under real use.
+  </MonoCard>
+)
+
+export default About
